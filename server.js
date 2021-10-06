6e738ee35384ae364d141065eadb0b23039ff3fc
Hugo <HF77554@gmail.com>
2021-10-06 13:55:39 -0400

    HTML body switched to variable ref. at server.js

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.set('view engine', "ejs")
 app.use('/articles', articleRouter)
 
 app.get('/', (req,res)=> {
-    res.render('index')
+    res.render('index', {text: 'Hello'} )
 })
 app.listen(5000)
